6c1663e6863e909394101833073db36509233bd5
Mary Hogan <maryh0826@gmail.com>
2020-07-15 16:03:03 -0400

    fixed mem - num regs separate from total reg size

--- cms_ilp.py
+++ cms_ilp.py
@@ -5,7 +5,6 @@ import time
 
 # input will be something like:
 #	N = total number of actions in unrolled p4 program
-#	deps = dict{tuple:dep} [1 = ordered dep, 2 = unordered dep]
 #	stateful = [list of indices of stateful actions]
 #	groups = [list of [groups] that must occur together]
 
@@ -14,32 +13,33 @@ start_time = time.time()
 
 # input from unrolled p4 program:
 # N value (upper bound on num actions)
-N = 10
-# dependencies
-deps = {(0,5):1, (1,6):1, (2,7):1, (3,8):1, (4,9):1, (5,6):2, (5,7):2, (5,8):2, (5,9):2, (6,7):2, (6,8):2, (6,9):2, (7,8):2, (7,9):2, (8,9):2}
+N = 12
 # list of stateful action indices
-stateful = [0,1,2,3,4]
+stateful = [0,1,2,3,4,5,6,7,8,9,10]
 # list of actions w/ corresponding metadata
-meta = [0,1,2,3,4]
+meta = [0,1,2,3,4,5,6,7,8,9,10]
 # amount of metadata per action
 meta_per = 64
 # list of groups
-groups = [[0,5], [1,6], [2,7], [3,8], [4,9]]
+groups = [[0,11], [1,12], [2,13], [3,14], [4,15], [5,16], [6,17], [7,18], [8,19], [9,20], [10,21]]
 # list of which actions are in same loop
-loops = [[0,1,2,3,4], [5,6,7,8,9]]
+loops = [[0,1,2,3,4,5,6,7,8,9,10], [11,12,13,14,15,16,17,18,19,20,21]]
+
 
 
 # switch resources input:
 # total available stateful memory
-total_mem = 2048
+total_mem = 4096
 # lower bound for reg size
 reg_bound = 256
 # num stages
-num_stg = 3
+num_stg = 12
 # stateful actions per stg
-num_state = 5
+num_state = 4
 # phv
 phv = 4096
+# size of each register in reg array (size of items store in regs in bits)
+item_size = 32
 
 # Model
 m = Model("test")
@@ -50,7 +50,8 @@ stages_vars = []	# list for each stage, composed of STATEFUL actions for each st
 for i in range(num_stg):
 	stages_vars.append([])
 # decision variable for every (action num, stg num) pair
-for i in range(N):
+# we multiply N by 2 bc we have 2 actions to consider (hash and min calc)
+for i in range(N*2):
 	act_vars.append([])
 	for j in range(num_stg):
 		act_vars[i].append(m.addVar(vtype=GRB.BINARY,name="%s%s"%(i,j)))
@@ -58,25 +59,6 @@ for i in range(N):
 			stages_vars[j].append(act_vars[i][j])
 
 	m.addConstr(quicksum(act_vars[i])<=1)	# only place action once
-
-# dep constraints
-for key in deps:
-	a0 = key[0]
-	a1 = key[1]
-	if deps[key] == 1:	# ordered
-		for x in range(len(act_vars[a0])):
-			z = 0
-			while z <= x:
-				m.addConstr(act_vars[a1][z]<=(1-act_vars[a0][x]))
-				z += 1
-
-	elif deps[key] == 2:	# unordered
-		for x in range(len(act_vars[a0])):
-			m.addConstr(act_vars[a0][x]<=(1-act_vars[a1][x]))
-
-        elif deps[key] == 3:    # same stg
-                for x in range(len(act_vars[a0])):
-                        m.addConstr(act_vars[a0][x] == act_vars[a1][x])
 
 
 # stateful action constraint
@@ -112,24 +94,25 @@ for l in stateful:
 	stg_count = 0
 	mem_vars.append([])
 	for i in range(len(act_vars[l])):
-		mem_vars[j].append(m.addVar(lb=0,vtype=GRB.INTEGER,name="mem%s%s"%(l,stg_count)))
+		mem_vars[j].append(m.addVar(lb=0,ub=total_mem/item_size,vtype=GRB.INTEGER,name="mem%s%s"%(l,stg_count)))
 		stages_mem_vars[i].append(mem_vars[j][-1])
-		m.addConstr(mem_vars[j][-1] <= act_vars[l][i]*total_mem)	# actions not allocated have 0 memory
-		m.addConstr(mem_vars[j][-1] >= act_vars[l][i])		# actions in stgs have nonzero memory allocation
+		m.addConstr(mem_vars[j][-1]*item_size <= act_vars[l][i]*total_mem)	# actions not allocated have 0 memory
+		m.addConstr(mem_vars[j][-1]*item_size >= act_vars[l][i])		# actions in stgs have nonzero memory allocation
 		stg_count += 1
 	#act_count += 1
 
 for l in stages_mem_vars:
-	m.addConstr(quicksum(l) <= total_mem)			# memory allocated in each stg adheres to memory constraints
+	m.addConstr(quicksum(l)*item_size <= total_mem)			# memory allocated in each stg adheres to memory constraints
 # constraint so each row gets SAME amount of memory - how to identify this in orig program/represent in graph?
 # all reg arrays contained in array in p4all program should get same amount memory
-'''
-for l in range(len(mem_vars)):
-	if l == 0:
+
+c = -1
+
+for me in mem_vars:
+	c+=1
+	if c == 0:
 		continue
-	m.addConstr(quicksum(mem_vars[(l-1)])-quicksum(mem_vars[l]) >= (1-quicksum(act_vars[l-1]))*(1-quicksum(act_vars[l]))*total_mem)
-        m.addConstr(quicksum(mem_vars[(l)])-quicksum(mem_vars[l-1]) <= (1-quicksum(act_vars[l-1]))*(1-quicksum(act_vars[l]))*(-total_mem))
-'''
+	m.addConstr(quicksum(me)*quicksum(act_vars[0])==quicksum(mem_vars[0])*quicksum(act_vars[c]))
 
 #m.addConstr(quicksum(mem_vars[0])-quicksum(mem_vars[1]) <= (quicksum(act_vars[0]))*(quicksum(act_vars[1]))*total_mem)
 #m.addConstr(quicksum(mem_vars[1])-quicksum(mem_vars[0]) >= (quicksum(act_vars[0]))*(quicksum(act_vars[1]))*(-total_mem))
@@ -154,18 +137,22 @@ m.addConstr(quicksum(meta_vars)*meta_per <= phv)
 
 
 # Objective to optimize
-m.setObjective(quicksum(x for i in act_vars for x in i), GRB.MAXIMIZE)
+#m.setObjective(2/quicksum(mem_vars[0]), GRB.MINIMIZE)
+#m.setObjective(.75*quicksum(x for i in act_vars for x in i)+.25*quicksum(y for z in mem_vars for y in z), GRB.MAXIMIZE)
+#m.setObjective(quicksum(x for i in act_vars for x in i),GRB.MAXIMIZE)
 #m.setObjective(quicksum(x for i in mem_vars for x in i), GRB.MAXIMIZE)
+#m.setObjective(quicksum(x for i in act_vars for x in i), GRB.MAXIMIZE)
+m.setObjective(quicksum(y for z in mem_vars for y in z), GRB.MAXIMIZE)
 
 # Solve
 m.optimize()
 
 print("--- %s seconds ---" % (time.time() - start_time))
-
+#'''
 # print all variable values
 for v in m.getVars():
         print('%s %g' % (v.varName, v.x))
-
+#'''
 
 '''
 # memory constraint
